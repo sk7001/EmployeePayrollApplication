b4c7e791930709a633e973608343619f0eae8224
Srinivas Sobhit Kintali <sk7001@srmist.edu.in>
2025-05-24 12:58:14 +0530

    UC8: Configured database settings to use environment variables in application.properties

--- src/main/java/com/example/employeepayrollapp/model/Employee.java
+++ src/main/java/com/example/employeepayrollapp/model/Employee.java
@@ -15,7 +15,6 @@ import lombok.AllArgsConstructor;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String name;
     private double salary;
 }
